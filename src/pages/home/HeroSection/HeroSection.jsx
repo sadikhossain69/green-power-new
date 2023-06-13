561548ec04128fc2737b1b74465bb48eaac6f8a5
sadikhossain69 <sadikhossain433@gmail.com>
2023-06-13 15:12:41 +0600

    font change

--- src/pages/home/HeroSection/HeroSection.jsx
+++ src/pages/home/HeroSection/HeroSection.jsx
@@ -6,12 +6,12 @@ const HeroSection = () => {
     return (
         <>
             <div>
-                <section className="text-gray-600 body-font bg-white ">
+                <section className="text-gray-600 body-font bg-white">
                     <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col-reverse items-center">
                         <Fade left>
                             <div
                                 className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                                <h1 className={`title-font sm:text-4xl text-3xl mb-4 font-bold text-[#0000FF]`}>GREEN POWERED
+                                <h1 className={`title-font sm:text-4xl text-3xl mb-4 font-bold text-[#0000FF] akshar-font`}>GREEN POWERED
                                     <br className="hidden lg:inline-block" />SYSTEMS
                                 </h1>
                                 <p className="text-black font-semibold mb-3">
